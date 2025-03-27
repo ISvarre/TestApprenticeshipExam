efbeb8e390a80647e0469ca0867fb689aa989ef5
Isak Svarre <Isak.svarre@gmail.com>
2025-03-27 13:59:29 +0100

    Destination Card Display

--- ApprenticeshipExam/tailwind.config.js
+++ ApprenticeshipExam/tailwind.config.js
@@ -2,7 +2,9 @@
 module.exports = {
   content: [
     "./src/**/*.{vue,js,ts,jsx,tsx}", // Include all Vue and JS/TS files
-    "../Views/**/*.cshtml"
+    "../Views/**/*.cshtml",
+    "~/Views/**/*.cshtml",
+    "~/Views/StartPage/Index.cshtml"
   ],
   theme: {
     extend: {},
--- Models/Pages/StartPage.cs
+++ Models/Pages/StartPage.cs
@@ -1,6 +1,7 @@
 using System.ComponentModel.DataAnnotations;
-using PrøveFagprøve.Models.Blocks;
+using EPiServer;
 using EPiServer.SpecializedProperties;
+using PrøveFagprøve.Models.Blocks;
 
 namespace PrøveFagprøve.Models.Pages;
 
